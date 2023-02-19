ebf4bb2aa90cee4c3d9efa177ce4cd892e817c8f
tkeesara <tkeesara@gmail.com>
2023-02-19 10:04:23 +0000

    domain commit

--- 10-ALB-Path-Based-Routing/terraform-manifests/c6-02-datasource-route53-zone.tf
+++ 10-ALB-Path-Based-Routing/terraform-manifests/c6-02-datasource-route53-zone.tf
@@ -1,6 +1,6 @@
 # Get DNS information from AWS Route53
 data "aws_route53_zone" "mydomain" {
-  name         = "devopsincloud.com"
+  name         = "devopsincloudpractice.com"
 }
 
 # Output MyDomain Zone ID
